27d7281eee72c785287a5cfac483820fa69c4c64
andreasonny83 <andreasonny83@gmail.com>
2018-10-20 18:29:22 +0100

    :sparkles: Button component and base theme

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { ThemeProvider } from 'emotion-theming';
 import { log, LogLevelDesc } from './log';
 
 import './index.css';
+import { Button } from './components/button';
+
+const theme = {
+  Button: {
+    color: '#fff',
+    background: '#00a396',
+    colorDisabled: '##333',
+    backgroundDisabled: '#3d968f',
+    backgroundFocus: '#00c2b2',
+    backgroundActive: '#00a396',
+  },
+};
 
 interface IApp {
   name: string;
@@ -29,10 +42,18 @@ export const bootstrap = (appConfig: IAppConfig) => {
 
   return ReactDOM.render(
     <WishlistProvider.Provider value={appConfig}>
-      <h1>
-        <span>Allo!</span>
-        <span>Welcome to WishList</span>
-      </h1>
+      <ThemeProvider theme={theme}>
+        <main>
+          <h1>
+            <span>Allo!</span>
+            <span>Welcome to WishList</span>
+          </h1>
+          <Button className="start-button">Start</Button>
+          <Button disabled className="start-button">
+            Disabled
+          </Button>
+        </main>
+      </ThemeProvider>
     </WishlistProvider.Provider>,
     document.getElementById('root')
   );
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,40 +1,70 @@
 import * as React from 'react';
-import { WishlistProvider } from '../app';
+import styled, { cx } from 'react-emotion';
 
 interface ButtonProps {
-  onClick(e: React.MouseEvent<HTMLElement>): void;
-  color?: React.CSSProperties;
+  className?: string;
+  disabled?: boolean;
 }
 
-interface ButtonComponentProps {
-  name: string;
-}
-
-class CustomButton extends React.PureComponent<
-  ButtonProps & ButtonComponentProps
-> {
-  render() {
-    const { color, onClick: handleClick, name } = this.props;
-
-    return (
-      <button style={color} onClick={handleClick}>
-        {this.props.children} - {name}
-      </button>
-    );
-  }
-}
 export class Button extends React.PureComponent<ButtonProps> {
   render() {
-    const { color, onClick: handleClick, children } = this.props;
+    const { children, className, ...otherProps } = this.props;
 
     return (
-      <WishlistProvider.Consumer>
-        {({ app }) => (
-          <CustomButton color={color} onClick={handleClick} name={app.name}>
-            {children}
-          </CustomButton>
-        )}
-      </WishlistProvider.Consumer>
+      <StyledButton
+        className={cx('wishlist-button', className)}
+        {...otherProps}
+      >
+        {children}
+      </StyledButton>
     );
   }
 }
+
+export interface StyledButtonProps {
+  // override?: ButtonThemeProps;
+  color?: string;
+  roundCorners?: boolean;
+  backgroundFocus?: string;
+}
+
+const StyledButton = styled('button')<StyledButtonProps>`
+  border-radius: 4px;
+  margin: 0.5rem;
+  border-color: transparent;
+  background-color: ${(props) => props.theme.Button.background};
+  padding: 0.38rem 1.15rem;
+  color: ${(props) => props.theme.Button.color};
+  outline: 0;
+  font-size: 1.13rem;
+  font-weight: 500;
+  line-height: 1.83rem;
+  box-shadow: 0 2px 4px 0 rgba(14, 30, 37, 0.12);
+  cursor: pointer;
+  display: inline-block;
+  text-align: center;
+  transition: all 150ms ease;
+  vertical-align: middle;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  :focus,
+  :hover {
+    text-decoration: none;
+    background-color: ${(props) => props.theme.Button.backgroundFocus};
+  }
+
+  :active {
+    background-color: ${(props) => props.theme.Button.backgroundActive};
+    box-shadow: none;
+  }
+
+  :disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+    box-shadow: none;
+    pointer-events: none;
+  }
+`;
+
+StyledButton.defaultProps = {};
